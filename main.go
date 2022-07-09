fa8a2736ff9e131fe90b045600d37ee420ecf96e
atamir <assaf.tamir.94@gmail.com>
2022-07-09 11:35:56 +0300

    main

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
-package WordHash
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"time"
+)
+
+var words = make([]string, 10000, 10000*10000)
+var hash = make([]string, 1000)
+
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	log.Printf("%s took %s", name, elapsed)
+}
+
+func readFile() {
+	defer timeTrack(time.Now(), "readFile")
+	file, err := os.Open("words.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-
+	readFile()
 }
